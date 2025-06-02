3ca9efe77da6ba196574cf2edd0a547a5322aff6
mgpinf <manishgowd09@gmail.com>
2025-06-02 18:38:25 +0530

    fix: rename direction parameter to activate_match in ExtendedSearch

--- config/src/keyassignment.rs
+++ config/src/keyassignment.rs
@@ -599,7 +599,7 @@ pub enum KeyAssignment {
     Search(Pattern),
     ExtendedSearch {
         pattern: Pattern,
-        direction: ActivateMatchPosition,
+        activate_match: ActivateMatchPosition,
     },
     ActivateCopyMode,
 
