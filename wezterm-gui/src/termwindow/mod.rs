d88553ad7e304a49a6125f3af9f31d32a65610e5
mgpinf <manishgowd09@gmail.com>
2025-06-02 18:55:42 +0530

    feat: implement Default trait for ActivateMatchPosition enum in copy

--- wezterm-gui/src/termwindow/mod.rs
+++ wezterm-gui/src/termwindow/mod.rs
@@ -2870,7 +2870,7 @@ impl TermWindow {
                     if let Some(existing) = pane.downcast_ref::<CopyOverlay>() {
                         let mut params = existing.get_params();
                         params.editing_search = true;
-                        params.activate_match_pos = ActivateMatchPosition::First;
+                        params.activate_match_pos = ActivateMatchPosition::default();
                         if !pattern.is_empty() {
                             params.pattern = self.resolve_search_pattern(pattern.clone(), &pane);
                         }
@@ -2883,7 +2883,7 @@ impl TermWindow {
                             CopyModeParams {
                                 pattern: self.resolve_search_pattern(pattern.clone(), &pane),
                                 editing_search: true,
-                                activate_match_pos: ActivateMatchPosition::First,
+                                activate_match_pos: ActivateMatchPosition::default(),
                             },
                         )?;
                         self.assign_overlay_for_pane(pane.pane_id(), search);
@@ -2987,7 +2987,7 @@ impl TermWindow {
                             CopyModeParams {
                                 pattern: MuxPattern::default(),
                                 editing_search: false,
-                                activate_match_pos: ActivateMatchPosition::First,
+                                activate_match_pos: ActivateMatchPosition::default(),
                             },
                         )?;
                         self.assign_overlay_for_pane(pane.pane_id(), copy);
