572e26da48128c829a88a3772317cb527528e9e2
mgpinf <manishgowd09@gmail.com>
2025-06-07 00:22:18 +0530

    feat: add JumpToMatchingBracket CopyMode KeyAssignment
    The above KeyAssignment applies to the bracket pairs `{}`, `()`, `[]`

--- config/src/keyassignment.rs
+++ config/src/keyassignment.rs
@@ -845,6 +845,7 @@ pub enum CopyModeAssignment {
     JumpBackward { prev_char: bool },
     JumpAgain,
     JumpReverse,
+    JumpToMatchingBracket,
 }
 
 pub type KeyTable = HashMap<(KeyCode, Modifiers), KeyTableEntry>;
