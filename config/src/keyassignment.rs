364a63d55b1284a552cf4b4009fa92e9fab5d871
mgpinf <manishgowd09@gmail.com>
2025-08-02 11:43:34 +0530

    feat: add option to start SelectorActions overlay in fuzzy mode
    Add boolean field `fuzzy` in SelectorActions KeyAssignment which
    defaults to false. If set to true, the SelectorActions overlay starts in
    filtering mode

--- config/src/keyassignment.rs
+++ config/src/keyassignment.rs
@@ -633,6 +633,8 @@ pub struct SelectorActions {
     #[dynamic(default)]
     pub fuzzy_description: Option<String>,
     #[dynamic(default)]
+    pub fuzzy: bool,
+    #[dynamic(default)]
     pub cancel: Option<Box<KeyAssignment>>,
 }
 
