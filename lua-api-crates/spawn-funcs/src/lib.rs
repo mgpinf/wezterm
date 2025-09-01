60769213db6bd009918c0f2fccdc5e6007dda127
mgpinf <manishgowd09@gmail.com>
2025-09-01 17:07:10 +0530

    feat(lua-api): add run_child_process_extended method to lua API
    run_child_process_extended takes below arguments:
    * args
    * cwd
    * set_environment_variables

--- lua-api-crates/spawn-funcs/src/lib.rs
+++ lua-api-crates/spawn-funcs/src/lib.rs
@@ -1,6 +1,20 @@
+use std::collections::HashMap;
+use std::path::PathBuf;
+
 use bstr::BString;
 use config::lua::get_or_create_module;
 use config::lua::mlua::{self, Lua};
+use luahelper::impl_lua_conversion_dynamic;
+use wezterm_dynamic::{FromDynamic, ToDynamic};
+
+#[derive(Debug, FromDynamic, ToDynamic, Clone)]
+struct ExtendedChildProcess {
+    args: Vec<String>,
+    cwd: Option<PathBuf>,
+    set_environment_variables: Option<HashMap<String, String>>,
+}
+
+impl_lua_conversion_dynamic!(ExtendedChildProcess);
 
 pub fn register(lua: &Lua) -> anyhow::Result<()> {
     let wezterm_mod = get_or_create_module(lua, "wezterm")?;
@@ -12,6 +26,10 @@ pub fn register(lua: &Lua) -> anyhow::Result<()> {
     wezterm_mod.set(
         "background_child_process",
         lua.create_async_function(background_child_process)?,
+    )?;
+    wezterm_mod.set(
+        "run_child_process_extended",
+        lua.create_async_function(run_child_process_extended)?,
     )?;
     Ok(())
 }
@@ -70,4 +88,38 @@ async fn background_child_process<'lua>(_: &'lua Lua, args: Vec<String>) -> mlua
         .map_err(mlua::Error::external)?;
 
     Ok(())
+}
+
+async fn run_child_process_extended<'lua>(
+    _: &'lua Lua,
+    extended_child_process: ExtendedChildProcess,
+) -> mlua::Result<(bool, BString, BString)> {
+    let args = &extended_child_process.args;
+    let mut cmd = smol::process::Command::new(&args[0]);
+
+    if args.len() > 1 {
+        cmd.args(&args[1..]);
+    }
+
+    if let Some(env_vars) = extended_child_process.set_environment_variables.as_ref() {
+        cmd.envs(env_vars);
+    }
+
+    if let Some(cwd) = extended_child_process.cwd.as_ref() {
+        cmd.current_dir(cwd);
+    }
+
+    #[cfg(windows)]
+    {
+        use smol::process::windows::CommandExt;
+        cmd.creation_flags(winapi::um::winbase::CREATE_NO_WINDOW);
+    }
+
+    let output = cmd.output().await.map_err(mlua::Error::external)?;
+
+    Ok((
+        output.status.success(),
+        output.stdout.into(),
+        output.stderr.into(),
+    ))
 }
