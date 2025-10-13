18f886f3219568a02e5d66b590385feb6000f380
mgpinf <manishgowd09@gmail.com>
2025-10-13 11:37:11 +0530

    fix: render line under description in transient_separator_fg

--- wezterm-gui/src/overlay/transient.rs
+++ wezterm-gui/src/overlay/transient.rs
@@ -222,7 +222,9 @@ impl SelectorState<'_> {
                         Change::Text(self.description.to_string()),
                         Change::AllAttributes(CellAttributes::default()),
                         Change::Text("\r\n".to_string()),
+                        Change::Attribute(AttributeChange::Foreground(self.colors.separator_fg)),
                         Change::Text("─".repeat(description_len)),
+                        Change::Attribute(AttributeChange::Foreground(ColorAttribute::Default)),
                     ]);
 
                     self.buf.draw_from_screen(&description_surface, 0, 0);
