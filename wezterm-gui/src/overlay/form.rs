95af3aee012d8b31426b44fb6ec4d7c36e87a722
mgpinf <manishgowd09@gmail.com>
2025-12-21 12:13:58 +0530

    feat: Enter always attempts submit in InputForm

--- wezterm-gui/src/overlay/form.rs
+++ wezterm-gui/src/overlay/form.rs
@@ -224,22 +224,18 @@ impl<'a> FormState<'a> {
                     key: KeyCode::Enter,
                     ..
                 }) => {
-                    if self.active_idx == self.args.fields.len() - 1 {
-                        let mut valid = true;
-                        for (idx, field) in self.args.fields.iter().enumerate() {
-                            if field.required && self.field_values[idx].trim().is_empty() {
-                                self.active_idx = idx;
-                                valid = false;
-                                break;
-                            }
-                        }
-
-                        if valid {
-                            self.submit();
+                    let mut valid = true;
+                    for (idx, field) in self.args.fields.iter().enumerate() {
+                        if field.required && self.field_values[idx].trim().is_empty() {
+                            self.active_idx = idx;
+                            valid = false;
                             break;
                         }
-                    } else {
-                        self.active_idx += 1;
+                    }
+
+                    if valid {
+                        self.submit();
+                        break;
                     }
                 }
                 InputEvent::Key(KeyEvent {
