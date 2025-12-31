8a29db1005dfe996081c652d91622b401420af86
mgpinf <manishgowd09@gmail.com>
2025-12-31 14:43:33 +0530

    feat: add ToggleFloatingPane KeyAssignment
    Add a new key assignment to toggle the visibility of the floating pane.
    If visible, it hides the pane; if hidden, it shows it. Does nothing if
    there is no floating pane. New floating panes are always shown.

--- config/src/keyassignment.rs
+++ config/src/keyassignment.rs
@@ -720,6 +720,7 @@ pub enum KeyAssignment {
     SpawnCommandInNewTab(SpawnCommand),
     SpawnCommandInNewWindow(SpawnCommand),
     SpawnCommandInFloatingPane(FloatingPaneSpawn),
+    ToggleFloatingPane,
     SplitHorizontal(SpawnCommand),
     SplitVertical(SpawnCommand),
     ShowLauncher,
