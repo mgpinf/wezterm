db3590af4ec8d66f2ad6fa5fd381ce3f1762fce0
mgpinf <manishgowd09@gmail.com>
2026-04-12 18:40:19 +0530

    selector_actions: add g, G navigation keys
    Add g key to navigate to first filtered entry if present
    Add G key to navigate to last filtered entry if present
    
    Each of the above keys only take effect if not in filtering mode and it
    is not a pending character

--- wezterm-gui/src/overlay/selector_actions.rs
+++ wezterm-gui/src/overlay/selector_actions.rs
@@ -584,6 +584,32 @@ impl<'a> SelectorState<'a> {
                     continue;
                 }
                 InputEvent::Key(KeyEvent {
+                    key: KeyCode::Char('g'),
+                    modifiers: Modifiers::NONE,
+                }) if !self.keymap.has_continuation(&self.typed, 'g') => {
+                    if let Some(first_entry) = self.filtered_entries.first() {
+                        self.active_idx = first_entry.idx;
+                        if self.active_idx < self.top_row {
+                            self.top_row = self.active_idx;
+                        }
+                    } else {
+                        continue;
+                    }
+                }
+                InputEvent::Key(KeyEvent {
+                    key: KeyCode::Char('G'),
+                    modifiers: Modifiers::SHIFT,
+                }) if !self.keymap.has_continuation(&self.typed, 'G') => {
+                    if let Some(last_entry) = self.filtered_entries.last() {
+                        self.active_idx = last_entry.idx;
+                        if self.active_idx > self.top_row + self.max_items {
+                            self.top_row = self.active_idx.saturating_sub(self.max_items);
+                        }
+                    } else {
+                        continue;
+                    }
+                }
+                InputEvent::Key(KeyEvent {
                     key: KeyCode::Char(c),
                     modifiers: Modifiers::NONE,
                 }) if c.is_ascii_digit() && !self.keymap.has_continuation(&self.typed, c) => {
